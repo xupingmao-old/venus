bc9979cae88c554ce233924ce016bbc228418b11
xupingmao <xupingmao@gmail.com>
2014-09-27 21:26:47 +0800

    add test_map.c to git

---
+++ vm/test/test_map.c
@@ -0,0 +1,29 @@
+
+#include "../tm.h"
+
+void test_map(){
+	tm_obj map = map_new();
+	tm_set(map, S("abc"), N(123));
+	tm_set(map, S("bcd"), N(456));
+	tm_set(map, S("def"), N(34.34));
+	tm_set(map, S("abc"), S("test"));
+	tm_obj k,v;
+	while( map_inext( map.value.map, &k, &v)){
+		cprint(k);
+		printf(":");
+		cprint(v);
+	}
+	// cprint(map);
+	// printf("\n\n\n");
+	tm_set( map, S("aaa"), S("tetss"));
+	tm_set( map, S("bbb"),  S("jfjjjj"));
+	tm_set( map, S("ccc"), S("value32"));
+	tm_set( map, S("ggg"), S("tetss"));
+	tm_set( map, S("abc"),  S("jfjjjj"));
+	tm_set( map, S("ddd"), S("value322"));
+
+	// cprint(map);
+
+	tm_obj mm = map_new();
+	tm_set(mm , S("testfor"), S("testwhile"));
+}
