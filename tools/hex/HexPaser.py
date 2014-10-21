602bfcbc417260d5c38a561e91f8a9fb885d0302
xupingmao <xupingmao@gmail.com>
2014-10-21 22:51:40 +0800

    HexPaser done!

--- tools/hex/HexPaser.py
+++ tools/hex/HexPaser.py
@@ -1,4 +1,55 @@
 
+numbers = "1234567890"
+alphas = "abcdefghijklmnopqrstuvwxyz"
+symbols = "`!@#$%^&*(){}_+=-[]\\|:;'\"<>?,./ "
+chars = numbers + alphas + alphas.upper() + symbols
 class HexPaser:
 
-    def load(
+    def __init__(self, fp = None, fname = None):
+        if fp == None and fname == None:
+            return
+        if fp == None:
+            fp = open( fname, "rb")
+            self.txt = fp.read()
+            fp.close()
+        if fname == None:
+            fname = fp.name
+            self.txt = fp.read()
+        self.fp = fp
+        self.name = fname
+        # close outside because it is not open here
+        # fp.close()
+
+    def getResult(self):
+        des = ""
+        count = 0
+        temp = ""
+        size = 20
+        def hexchar(c):
+            return hex(ord(c))[2:]
+        line = ""
+        for c in self.txt:
+            if c in chars:
+                temp+=c
+            else:
+                temp+='.'
+            line += hexchar(c).center(3)
+            count+=1
+            if count % size == 0:
+                des+= line + ' '* 5 + temp
+                temp=""
+                line = ""
+                des+='\n'
+        if line != "":
+           des += line.ljust(size * 3) + ' '*5 + temp 
+        return des
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 2:
+        fname = sys.argv[1]
+    else:
+        print("need filename")
+        exit(0) 
+    x = HexPaser(fname = fname)
+    print(x.getResult())
--- tools/hex/Reader.py
+++ tools/hex/Reader.py
@@ -1,17 +1,41 @@
 import Tkinter, tkFileDialog, tkFont
-
+from HexPaser import HexPaser
+import os
 class App():
     
     def load(fname):
         return open(fname,'rb').read()
 
-    def openfile(self):
-        fp = tkFileDialog.askopenfile()
-        self.textView.delete(0.0 , Tkinter.END)
-        self.txt = fp.read()
-        self.charslen = len(self.txt)
+    # update filename and filetext
+    def update(self):
+        self.textView.delete(0.0, Tkinter.END)
         self.textView.insert(0.0, self.txt)
-        
+        title = self.title + ' -- ' + self.name
+        self.root.title(title)
+        # bottom text
+        btext = self.name[:50]
+        btext = btext.ljust(50)
+        btext += '    ' + str(os.path.getsize(self.name))+' byte'
+        self.filelabel.config( text = btext)
+    def openfile(self):
+        # this open mode is proberly r, not rb
+        fp = tkFileDialog.askopenfile()
+        # do not select any file
+        if fp == None:
+            return
+        self.name = fp.name
+        x = HexPaser(fname = fp.name)
+        self.txt = x.getResult()
+        fp.close()
+        self.update()
+    def openastext(self):
+        fp = tkFileDialog.askopenfile()
+        if fp == None:
+            return
+        self.txt = fp.read()
+        self.name = fp.name
+        fp.close()
+        self.update()
 
     def scale_content(sc):
         val = int(sc)
@@ -19,14 +43,25 @@ class App():
 
     def filemenu(self):
         filebtn = Tkinter.Menu(self.menuframe)
-        filebtn.add_command(label="open",command=self.openfile)
+        filebtn.add_command(label="open as hex",command=self.openfile)
+        filebtn.add_command(label="open as text", command=self.openastext)
+        filebtn.add_command(label="exit",command=self.exit) 
         return filebtn
+    def exit(self):
+        exit(0)
+    def exitmenu(self):
+        btn = Tkinter.Menu(self.menuframe)
+        btn.add_command( label = "exit", command = self.exit )
+        return btn
 
-    def __init__(self, title = 'Example'):
+    def __init__(self, title = 'Example', convertTab = True):
+        self.title = title
+        self.convertTab = convertTab
         self.charslen = 0.0
         root = Tkinter.Tk()
-        root.minsize(600, 500)
-        root.maxsize(600, 500)
+        width = 900
+        root.minsize(width, 500)
+        root.maxsize(width, 500)
         root.title(title)
         self.root = root
         # menu
@@ -42,10 +77,9 @@ class App():
         self.font = tkFont.Font(family = 'Consolas', size = 10)
         
         textView = Tkinter.Text(mainframe, wrap=Tkinter.WORD,
-                                highlightthickness=0)
+                                highlightthickness=0, width=width)
         textView.config( background ='black', foreground = 'green',
                          insertbackground='white', font = self.font)
-        
         
         scale = Tkinter.Scrollbar(mainframe, orient = Tkinter.VERTICAL,
                                   command = textView.yview)
@@ -54,6 +88,13 @@ class App():
         textView.config( yscrollcommand=scale.set)
         textView.pack(side = Tkinter.LEFT)
         self.textView = textView
+
+        # bottom information
+        self.bottom = Tkinter.Frame(root)
+        self.bottom.pack(fill = Tkinter.X, side = Tkinter.BOTTOM)
+        filelabel = Tkinter.Label(self.bottom, text = 'file')
+        filelabel.pack(side = Tkinter.LEFT)
+        self.filelabel = filelabel
         
     #  mainloop
     def run(self):
