5c26b49945ad41177f05fb4bb94b87ba5b3d80c5
xupingmao <xupingmao@gmail.com>
2014-11-02 00:22:34 +0800

    lost files

---
+++ vm/_boot.py
@@ -0,0 +1,48 @@
+def str_ljust(self, num):
+    if len(self) >= num: return self
+    rest = num - len(self)
+    return self + rest * ' '
+
+add_type_method('str', 'ljust', str_ljust)
+
+def str_rjust(self, num):
+    if len(self) >= num: return self
+    rest = num - len(self)
+    return rest * ' ' + self
+add_type_method('str', 'rjust', str_rjust)
+
+def _import(fname, tar = None):
+    if fname in __modules__:
+        # print("module "+fname+" already loaded");
+        pass
+    else:
+        # print("load module "+fname)
+        from encode import *
+        txt = load(fname)
+        # print("src:\n"+txt)
+        _code = b_compile(fname)
+        save(fname+".tmc", _code)
+        load_module(fname, _code)
+    g = __modules__[fname]
+    if tar == '*':
+        lg = get_last_frame_globals()
+        for k in g:
+            lg[k] = g[k]
+
+__builtins__['_import'] = _import
+def _execute_file(fname):
+    if not fname.endswith(".pyc"):
+        print("not python file")
+        return
+    fname = fname.substring(0,-)
+
+
+def _run_code(fname):
+    txt = load(fname)
+    _run(txt)
+
+def _eval( fname ):
+    from encode import *
+    txt = load(fname)
+    _code = b_compile(fname)
+    _run(_code)
---
+++ vm/test.py
@@ -0,0 +1,7 @@
+
+print("test list ")
+print([3,4,5])
+
+omap = {"test": "4","age":"3434"}
+print(omap)
+print("hello,world")
